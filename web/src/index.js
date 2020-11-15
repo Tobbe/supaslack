8fa811d7c41dac42b922f5adfebd25a4f3338023
Tobbe Lundberg <tobbe@tlundberg.com>
2020-11-15 22:33:08 +0100

    yarn rw g auth supabase + supabase downgrade to 0.36.5

--- web/src/index.js
+++ web/src/index.js
@@ -1,3 +1,5 @@
+import { AuthProvider } from '@redwoodjs/auth'
+import { createClient } from '@supabase/supabase-js'
 import ReactDOM from 'react-dom'
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
@@ -6,11 +8,18 @@ import Routes from 'src/Routes'
 
 import './index.css'
 
+const supabaseClient = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+  )
+
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider>
-      <Routes />
-    </RedwoodProvider>
+    <AuthProvider client={supabaseClient} type="supabase">
+      <RedwoodProvider>
+        <Routes />
+      </RedwoodProvider>
+    </AuthProvider>
   </FatalErrorBoundary>,
   document.getElementById('redwood-app')
 )
